f26a780cad47985a4cc7d749ce61cf905bf64987
hykim6856 <hykim6856@naver.com>
2024-03-08 09:11:09 +0900

    u

--- next-js-book-102/src/api/naver_api.js
+++ next-js-book-102/src/api/naver_api.js
@@ -1,24 +1,36 @@
+"use server";
+// const NAVER_CLIENT_ID = process.env.NAVER_CLIENT_ID;
+// const NAVER_CLIENT_SECRET = process.env.NAVER_CLIENT_SECRET;
+
+// console.log(NAVER_CLIENT_ID, NAVER_CLIENT_SECRET);
+
+// const books = await getNaverBooks();
+// const viewList = books.map((book) => <li>{book.title}</li>);
+// console.log(books);
+
+// process.env 환경변수를 구조분해하여 개별변수에 할당하기
 const { NAVER_CLIENT_ID, NAVER_CLIENT_SECRET } = process.env;
 const NAVER_BOOK_URL =
   "https://openapi.naver.com/v1/search/book.json";
 
-//만약 client mode 로 사용하는 컴포넌트, 함수모듈등에는
-//함수의 시작부분에 async 를 절대 사용해서는 안된다.
+// 만약 client mode 로 사용하는 컴포넌트, 함수모듈등에는
+// 함수의 시작부분에 async 를 절대 사용하면 안된다.
+
 export const getNaverBooks = async (search) => {
   const fetchOption = {
-    method: "GET",
+    nethod: "GET",
     headers: {
       "X-Naver-Client-Id": NAVER_CLIENT_ID,
       "X-Naver-Client-Secret": NAVER_CLIENT_SECRET,
     },
   };
-
   const response = await fetch(
     `${NAVER_BOOK_URL}?query=${search || "java"}`,
     fetchOption
   );
   const books = await response.json();
-  // naver api.js 는 서버 모듈이 모듈에서 콘솔로그를 사용하면 콘솔에 출력된다.
-  console.log("BOOKS", books);
+  // naver_api.js 는 server 모듈이다
+  // 이 모듈에서 console.log() 를 사용하면 서버 consol에 출력된다.
+  // console.log("BOOKS", books);
   return books.items;
 };
--- next-js-book-102/src/app/page.js
+++ next-js-book-102/src/app/page.js
@@ -1,19 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import BookMain from "@/comps/BookMain";
-// const NAVER_CLIENT_ID = process.env.NAVER_CLIENT_ID;
-// const NAVER_CLIENT_SECRET = process.env.NAVER_CLIENT_SECRET;
-
-//process.env 환경변수를 구조분해하여 개별 변수에 할당하기
-// const { NAVER_CLIENT_ID, NAVER_CLIENT_SECRET } = process.env;
-// const NAVER_BOOK_URL =
-//   "https://openapi.naver.com/v1/search/book.json";
 
 export default async () => {
-  // console.log(NAVER_CLIENT_ID, NAVER_CLIENT_SECRET);
-  // const books = await getNaverBooks();
-  // const viewList = books.map((book) => <li>{book.title}</li>);
-  // console.log(books);
   return (
     <main>
       <BookMain />
